f0f519eef9df859517d404809f5db834fc6c7003
Maxim <raincoven@gmail.com>
2016-12-20 17:03:09 +0300

    Generated: story1 steps

---
+++ EuromoneySeleniumTest/Steps/BaseSteps.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+
+namespace EuromoneySeleniumTest.Steps
+{
+    [Binding]
+    public class BaseSteps
+    {
+        readonly IWebDriver driver;
+
+
+        public BaseSteps()
+        {
+            driver = (IWebDriver)ScenarioContext.Current["driver"];
+        }
+
+
+    }
+}
