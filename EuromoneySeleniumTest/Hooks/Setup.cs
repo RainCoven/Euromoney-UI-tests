a74a0aa772327d6e34d3bd58a84039cea26d82dd
Maxim <raincoven@gmail.com>
2016-12-20 17:01:12 +0300

    Added: test run hook

---
+++ EuromoneySeleniumTest/Hooks/Setup.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace EuromoneySeleniumTest.Hooks
+{
+    [Binding]
+    public sealed class Setup
+    {
+        static IWebDriver driver;
+        //static string baseurl = ConfigurationManager.AppSettings["baseUrl"];
+
+        [BeforeScenario]
+        public static void BeforeWebScenario()
+        {
+            driver = new ChromeDriver();
+            ScenarioContext.Current["driver"] = driver;
+        }
+
+        [AfterScenario]
+        public static void AfterWebScenario()
+        {
+            driver.Quit();
+        }
+    }
+       
+}
